0004215418c54a3748c8e0990deba80dd94843d6
VikasNayak405 <vkumar9173731@gmail.com>
2022-03-26 01:51:50 +0530

    first commit

---
+++ Demo.js
@@ -0,0 +1,39 @@
+const http = require("http");
+const fs = require("fs");
+var requests = require("requests");
+
+const homeFile = fs.readFileSync("./Demo.html", "utf8");
+const replaceVal = (tempVal, orgval) => {
+ let temmperature = tempVal.replace("{%tempval%}", orgval.main.temp);
+ temmperature = temmperature.replace("{%tempmin%}", orgval.main.temp_min/10);
+ temmperature = temmperature.replace("{%tempmax%}", orgval.main. temp_max/10);
+ temmperature = temmperature.replace("{%location%}", orgval.name);
+ temmperature = temmperature.replace("{%country%}", orgval.sys.country);
+ temmperature = temmperature.replace("{%tempstatus%}", orgval.weather[0].main);
+
+ return temmperature;
+}
+const server = http.createServer((req,res)=>{
+  if(req.url == "/"){
+    requests('https://api.openweathermap.org/data/2.5/weather?q=orissa&units=metric&appid=893e529b235dcc2d5b4ffff75da37d20')
+.on('data',  (chunk) => {
+  const objdata = JSON.parse(chunk)
+  const arrData = [objdata];
+  //console.log(arrData[0].main.temp);
+  const realTimeData = arrData.map((val) => replaceVal(homeFile,val)).join("");
+
+   res.write(realTimeData);
+  // console.log(realTimeData);
+
+  
+})
+.on('end',  (err) => {
+  if (err) return console.log('connection closed due to errors', err);
+ 
+  //console.log('end');
+  res.end();
+});
+  }
+});
+
+server.listen(4000, "127.0.0.1");
